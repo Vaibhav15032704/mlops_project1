450521eb1571fcc9eef7f4446bb5afee4154af1e
Vaibhav15032704 <vaibhavdeshmukh5121@gmail.com>
2026-07-09 14:17:03 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vaibhav Deshmukh",
+    author_email="vaibhavddeshmukh5121@gmail.com",
+    packages=find_packages()
+)
